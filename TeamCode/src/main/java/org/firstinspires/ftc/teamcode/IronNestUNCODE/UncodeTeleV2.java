87667ca66be5ad3d7f5f55a001f0468fec9bef36
Ryan Guy Edimo <144562493+Westhighroboticryanedimo@users.noreply.github.com>
2025-11-25 22:38:40 -0900

    Vision portal introductions, Example  code is being used and tested, tweaked until eventually it works the way  we want to

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronNestUNCODE/UncodeTeleV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronNestUNCODE/UncodeTeleV2.java
@@ -6,11 +6,69 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.util.control.Controller;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.List;
+import java.util.Locale;
+
 @TeleOp(name="V2-tele")
-public class UncodeTeleV1 extends LinearOpMode{
+public class UncodeTeleV2 extends LinearOpMode{
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    /**
+     * The variable to store our instance of the AprilTag processor.
+     */
+    private AprilTagProcessor aprilTag;
+
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private VisionPortal visionPortal;
+    private void initAprilTag() {
+
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+
+    }
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format(Locale.US, "\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format(Locale.US, "XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format(Locale.US, "PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format(Locale.US, "RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format(Locale.US, "\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format(Locale.US, "Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }   // end method telemetryAprilTag()
     @Override
         public void runOpMode() {
 
@@ -29,6 +87,8 @@ public class UncodeTeleV1 extends LinearOpMode{
             FL.setDirection(DcMotorSimple.Direction.FORWARD);
             BL.setDirection(DcMotorSimple.Direction.FORWARD);
 
+            initAprilTag();
+
         Controller Gamepad1 = new Controller(gamepad1);
         
         Servo liftL = hardwareMap.get(Servo.class, "LiftL");
@@ -38,6 +98,8 @@ public class UncodeTeleV1 extends LinearOpMode{
 
             // run until the end of the match (driver presses STOP)
             while (opModeIsActive()) {
+
+                telemetryAprilTag();
                 double y = -Gamepad1.left_stick_y;
                 double x = Gamepad1.left_stick_x;
                 double rx = Gamepad1.right_stick_x;
@@ -63,8 +125,8 @@ public class UncodeTeleV1 extends LinearOpMode{
                     liftL.setPosition(0.01);
                     liftR.setPosition(0.99);
                 }else{
-                        liftL.setPosition(0.15);
-                        liftR.setPosition(0.85);
+                        liftL.setPosition(0.18);
+                        liftR.setPosition(0.7);
                 }
                 if(Gamepad1.left_trigger>.2||gamepad2.left_trigger>.2){
                     In.setPower(-1);
@@ -84,6 +146,7 @@ public class UncodeTeleV1 extends LinearOpMode{
                     OutR.setPower(0);
                 }
                 Gamepad1.update();
+                telemetry.update();
             }
 
 
