bb036713eaa2b3c1b842df9d0cd14034f33b4b64
Ryan Guy Edimo <144562493+Westhighroboticryanedimo@users.noreply.github.com>
2026-01-20 16:46:49 -0900

    Pedropathing Constants.java base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -4,7 +4,9 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.TwoWheelConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -12,9 +14,21 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(8.5);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
+    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
+            .strafePodX(8.66)
+            .forwardPodY(4)
+            .forwardEncoder_HardwareMapName("deadStrafe")
+            .strafeEncoder_HardwareMapName("deadForward")
+            .IMU_HardwareMapName("imu")
+            .IMU_Orientation(
+                    new RevHubOrientationOnRobot(
+                            RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                            RevHubOrientationOnRobot.UsbFacingDirection.UP
+                    )
+            );
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .twoWheelLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
                 .build();
